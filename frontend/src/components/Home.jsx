abf724e22197ce7497196d1b946d96ff5e28c72a
msingh7736 <msingh7763@gmail.com>
2025-12-01 11:32:18 +0530

    home created

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Home extends PureComponent {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Home
