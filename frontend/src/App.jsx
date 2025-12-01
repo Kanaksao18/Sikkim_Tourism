3af6b941d18847f3f904ff2bb0c7064a205972e5
Kanak <{ksao180205@gmail.com}>
2025-12-01 23:22:34 +0530

    Navbar Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+
+// Import your pages
+import Home from "./pages/Home";
+import Explore from "./pages/Explore";
+import AIItinerary from "./pages/AIItinerary";
+import Heritage from "./pages/Heritage";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/ai-itinerary" element={<AIItinerary />} />
+        <Route path="/heritage" element={<Heritage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
